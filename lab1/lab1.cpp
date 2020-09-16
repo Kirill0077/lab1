03d4e957ea9dad68bc8e46bfa43c75970b81b5e6
Kirill0077 <67799834+Kirill0077@users.noreply.github.com>
2020-09-16 15:28:44 +0300

    Добавьте файлы проекта.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+struct Truba
+{
+	float l;
+	string id;
+	int dim;
+	bool pz;
+};
+struct KS
+{
+	string id;
+	string name;
+	int cahi;
+	int ciw;
+	float proiz;
+};
+int main() 
+{
+
+}
