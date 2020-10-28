270c172b306749bc28264f61ac2ea2bd8bf15010
Kirill0077 <67799834+Kirill0077@users.noreply.github.com>
2020-10-28 17:34:50 +0300

    Сделана проверка входных данных Оптимизирован вывод информации из файла

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -14,37 +14,72 @@ struct KS
 {
 	string id = "1";
 	string name = "";
-	int shop = 0; 
-	int inwork = 0;
-	int KPD = 1;
+	unsigned int shop = 0; 
+	unsigned int inwork = 0;
+	unsigned int perfomance = 1;
 };
-
+int GetCorrectNumber(int min, int max)
+{
+	int x;
+	do
+	{
+		cin.clear();
+		cin.ignore(1000000, '\n');
+		cout << "Выберите пункт от "<<min <<" до "<<max<<": ";
+		cin >> x;
+	} while (cin.fail() || x<min || x>max); 
+	return x;
+}
+template <typename T>
+bool CheckPositiveValue(T x)
+{
+	return x > 0 ? true : false;
+}
 Truba CreatNewTruba()
 {	
-	Truba new_Truba;
+	Truba newTruba;
 	cout << "\n Cчитайте данные для трубы: " << endl;
-	new_Truba.id = "0";
-	new_Truba.repair = false;
-	cout << "\n введите длинну трубы (в м): ";
-	cin >> new_Truba.L;
-	cout << "\n введите диамтр трубы (в мм): ";
-	cin >> new_Truba.D;
-	return new_Truba;
+	newTruba.id = "0";
+	newTruba.repair = false;
+	do
+	{
+		cin.clear();
+		cin.ignore(1000000, '\n');
+		cout << "\n введите длинну трубы (в м): ";
+		cin >> newTruba.L;
+	} while (cin.fail() || !CheckPositiveValue(newTruba.L) );
+	do
+	{
+		cin.clear();
+		cin.ignore(1000000, '\n');
+		cout << "\n введите диамтр трубы (в мм): ";
+		cin >> newTruba.D;
+	} while (cin.fail() || !CheckPositiveValue(newTruba.D));
+	return newTruba;
 }
 KS CreatNewKS() 
 {
-	KS new_KS;
+	KS newKS;
 	cout << "\n Считайте данные для компрессарных станций: " << endl;
-	new_KS.id = "1";
+	newKS.id = "1";
 	cout << "\n Введите имя КС: ";
-	cin >> new_KS.name;
-	cout << "\n Введите количество цехов: ";
-	cin >> new_KS.shop;
-	cout << "\n Введите количество цехов в работе: " ;
-	cin >> new_KS.inwork;
-	cout << "\n Введите эффективность трубы  (1-10): ";
-	cin >> new_KS.KPD;
-	return new_KS;
+	cin.get();
+	getline(cin, newKS.name);
+	do
+	{
+		cin.clear();
+		cin.ignore(1000000, '\n');
+		cout << "\n Введите количество цехов: ";
+		cin >> newKS.shop;
+	} while (cin.fail());
+	do
+	{
+		cout << "\n Введите количество цехов в работе: ";
+		cin >> newKS.inwork;
+	} while (cin.fail() || newKS.inwork>newKS.shop);
+	cout << "\n Введите эффективность трубы. " << endl;
+	newKS.perfomance=GetCorrectNumber(1,10);
+	return newKS;
 
 }
 
@@ -68,7 +103,7 @@ void PrintKS(KS c)
 void SaveFileTruba(Truba t)
 {
 	ofstream fout;
-	fout.open("output.txt", ios::out);
+	fout.open("file.txt", ios::out);
 	if (fout.is_open())
 	{
 		fout << t.D << endl << t.id << endl << t.L << endl << t.repair << endl;
@@ -80,46 +115,34 @@ void SaveFileTruba(Truba t)
 void SaveFileKS(KS c)
 {
 	ofstream fout;
-	fout.open("output.txt", ios::app);
+	fout.open("file.txt", ios::app);
 	if (fout.is_open())
 	{
-		fout << c.id << endl << c.inwork << endl << c.KPD << endl << c.name << endl<<c.shop ;
+		fout << c.id << endl << c.inwork << endl << c.perfomance << endl << c.name << endl<<c.shop ;
 		fout.close();
 	}
 
 }
 
-Truba ReadFileTruba()
+Truba ReadFileTruba(ifstream& fin)
 {
-	ifstream fin;
-	fin.open("input.txt", ios::in);
 	Truba t;
-	if (fin.is_open())
-	{
-		fin >> t.D;
-		fin >> t.id;
-		fin >> t.L;
-		fin >> t.repair;
-		fin.close();
-		return t;
-	}
+	fin >> t.D;
+	fin >> t.id;
+	fin >> t.L;
+	fin >> t.repair;
+	return t;
 }
 
-KS ReadFileKS()
+KS ReadFileKS(ifstream& fin)
 {
-	ifstream fin;
-	fin.open("input.txt", ios::in);
 	KS c;
-	if (fin.is_open())
-	{
-		fin >> c.id;
-		fin >> c.inwork;
-		fin >> c.KPD;
-		fin >> c.name;
-		fin >> c.shop;
-		fin.close();
+	fin >> c.id;
+	fin >> c.inwork;
+	fin >> c.perfomance;
+	fin >> c.name;
+	fin >> c.shop;
 		return c;
-	}
 }
 void Menu()
 {
@@ -127,26 +150,59 @@ void Menu()
 	cout << "2. Создать Трубу" << endl;
 	cout << "3. Создать компрессарную станцию" << endl;
 	cout << "4. Просмотр всех объектов" << endl;
-	cout << "5. Редактировать трубу" << endl;
-	cout << "6. Запуски и остановка цеха" << endl;
+	cout << "5. Изменить статус трубу в ремонте/не в ремонте" << endl;
+	cout << "6. Запуск и остановка цеха" << endl;
 	cout << "7. Сохранить все данные в файл" << endl;
 	cout << "0. Выход" << endl;
 }
 
+void ChangeStatus(bool& status)
+{
+	status = !status;
+}
+void StopWork(KS& c) {
+	c.inwork--;
+}
+void StartWork(KS& c) {
+	c.inwork++;
+}
 int main() 
 {
 	setlocale(LC_ALL, "Russian");
 	Truba t;
 	KS c;
-	int i;
+	bool isTruba = false;
+	bool isKS = false;
 	while (true) 
 	{
 		Menu();
-		cin >> i;
-		switch (i)
+		switch (GetCorrectNumber(0,7))
 		{
 		case 1:
+		{
+			ifstream fin;
+			fin.open("file.txt", ios::in);
+			if (fin.is_open())
+			{
+				if (isTruba == true) 
+				{
+					t = ReadFileTruba(fin);
+				}
+				else
+				{
+					cout<<"Труба не создана"<<endl;
+				}
+				if (isKS == true) 
+				{
+					c = ReadFileKS(fin);
+				}
+				else
+				{
+					cout << "KC не создана" << endl;
+				}
+			}
 			break;
+		}
 		case 2:
 			t = CreatNewTruba();
 			break;
@@ -158,12 +214,43 @@ int main()
 			PrintKS(c);
 			break;
 		case 5:
+			ChangeStatus(t.repair);
 			break;
 		case 6:
-			break;
+			cout << "\t Выберите действие" << endl;
+			cout << "\t 1. Запуск цеха" << endl;
+			cout << "\t 2. Остановка цеха" << endl;
+			switch (GetCorrectNumber(1, 2))
+			{
+			case 1:
+				if (c.inwork < c.shop)
+				{
+					StartWork(c);
+				}
+				else
+				{
+					cout << "Все цеха заняты работой, выберите другой пнкт" << endl;
+				}
+				break;
+			case 2:
+				if (c.inwork > 0)
+				{
+					StopWork(c);
+				}
+				else
+				{
+					cout << "нет ни одного рабочего цеха, выберите другой пункт" << endl;
+				}
+				break;
+			default:
+				cout << "Произошла ошибка" << endl;
+				break;
+			}
 		case 7:
 			SaveFileTruba(t);
 			SaveFileKS(c);
+			isTruba = !isTruba;
+			isKS = !isKS;
 			break;
 		case 0:
 			return 0;
@@ -173,6 +260,4 @@ int main()
 			break;
 		}
 	}
-	t=ReadFileTruba();
-	PrintTruba(t);
 }
